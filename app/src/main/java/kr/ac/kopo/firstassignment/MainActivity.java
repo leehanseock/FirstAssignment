d56967b75939890cd158ab4627a8fbb13b413a1b
leehanseock <leehanseock@gmail.com>
2025-04-07 19:05:50 +0900

    배경화면 및 성명입력칸 추가

--- app/src/main/java/kr/ac/kopo/firstassignment/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/firstassignment/MainActivity.java
@@ -10,13 +10,14 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edit1, edit2;
+    EditText edit0, edit1, edit2;
     TextView textResult;
     ImageView imgView;
     Button btnCal;
@@ -32,19 +33,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        edit0 = findViewById(R.id.edit0);
         edit1 = findViewById(R.id.edit1);//객체 참조값 반환값 받기
         edit2 = findViewById(R.id.edit2);
         btnCal = findViewById(R.id.btn_cal);
         textResult = findViewById(R.id.text_result);
         imgView = findViewById(R.id.img_view);
         btnCal.setOnTouchListener(btnTouchListener);
+
+        applyFocusChangeListener(edit0);
+        applyFocusChangeListener(edit1);
+        applyFocusChangeListener(edit2);
+
+
     }
 
     View.OnTouchListener btnTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
+            String name = "";
+            String num0Str = edit0.getText().toString();
             String num1Str = edit1.getText().toString(); //getText()는 Editable 객체를 반환. String으로 쓰려면 toString() 사용.
             String num2Str = edit2.getText().toString();
+            String unit = getString(R.string.measure_result);
+            String bmiCategory;
+
+            if (num0Str.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "이름을 입력하세요", Toast.LENGTH_SHORT).show();
+                edit0.setFocusable(true);
+                return true;
+            } else {
+                name = num0Str;
+            }
 
             if (num1Str.isEmpty() || num2Str.isEmpty()) {
                 Toast.makeText(getApplicationContext(), "신장과 체중 모두 입력하세요", Toast.LENGTH_SHORT).show();
@@ -58,17 +78,44 @@ public class MainActivity extends AppCompatActivity {
 
             Button btnEvent = (Button) v;
 
-            try {
-                result = num2 / (heightInMeters * heightInMeters);
-            } catch (ArithmeticException e) {
-                Toast.makeText(getApplicationContext(), "0으로 나눌 수 없습니다", Toast.LENGTH_SHORT).show();
-                edit2.setText("");
-                edit2.setFocusable(true);
+            if (num1 <= 0 || num2 <= 0) {
+                Toast.makeText(getApplicationContext(), "신장과 체중을 모두 0보다 크게 입력하세요.", Toast.LENGTH_SHORT).show();
                 return true;
             }
-            textResult.setText(String.format("계산 결과: 당신의 BMI 지수는 %.1f입니다.",result));
+            else result = num2 / (heightInMeters * heightInMeters);
+
+            if (result < 18.5) {
+                bmiCategory = getString(R.string.bmi1); // 저체중
+                imgView.setImageResource(R.drawable.squarepants_character);
+            } else if (result < 24.9) {
+                bmiCategory = getString(R.string.bmi2); // 정상 체중
+                imgView.setImageResource(R.drawable.squidward_tentacles);
+            } else if (result < 29.9) {
+                bmiCategory = getString(R.string.bmi3); // 과체중
+                imgView.setImageResource(R.drawable.patrick_star_character);
+            } else {
+                bmiCategory = getString(R.string.bmi4); // 비만
+                imgView.setImageResource(R.drawable.pearl_the_whale);
+            }
+
+            String resultText = getString(R.string.result_format, name, result, unit, bmiCategory);
+            textResult.setText(resultText);
+
             return true;
         }
     };
+
+    private void applyFocusChangeListener(EditText editText) {
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    editText.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.shades));
+                } else {
+                    editText.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.title_color));
+                }
+            }
+        });
+    }
 
 }
